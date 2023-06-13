9826aac7810beef5dc9f9c49074e13b0bd83c706
Shubham Raut <shubhamrajkumar.raut@zemosolabs.com>
2023-06-13 13:40:55 +0530

    add chip atom

---
+++ frontend/src/components/atoms/chip/index.stories.tsx
@@ -0,0 +1,47 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import theme from "../../../theme";
+import ChipItem from './index'
+import React from 'react';
+
+export default {
+  title: 'Atoms/Chip',
+  component: ChipItem,
+} as ComponentMeta<typeof ChipItem>
+
+const Template: ComponentStory<typeof ChipItem> = (args) => (
+  <ChipItem {...args} />
+)
+
+export const ChipItemRoundedLight = Template.bind({})
+
+ChipItemRoundedLight.args = {
+  label: '24 h',
+  chipvariant: 'light',
+  chiptype: 'rounded'
+}
+
+export const ChipItemRoundedDark = Template.bind({})
+
+ChipItemRoundedDark.args = {
+  label: 'Purchased',
+  chipvariant: 'dark',
+  chiptype: 'rounded'
+}
+
+export const ChipItemSquaredSelected = Template.bind({})
+
+ChipItemSquaredSelected.args = {
+  chiptype: 'squared',
+  chipcolor: theme.palette.chipColors.main,
+  label: 'Bitcoin',
+  selected: true
+}
+
+export const ChipItemSquaredUnselected = Template.bind({})
+
+ChipItemSquaredUnselected.args = {
+  chiptype: 'squared',
+  chipcolor: theme.palette.chipColors.main,
+  label: 'Bitcoin',
+  selected: false
+}
