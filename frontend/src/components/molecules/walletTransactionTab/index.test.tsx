90d019b1e8e091f615ad087232dda43a230a79be
venkatalakshmimaddu <115541248+venkatalakshmimaddu@users.noreply.github.com>
2023-06-15 13:39:38 +0530

    feat(transactionTab):Add wallet transaction tab molecule (#29)

---
+++ frontend/src/components/molecules/walletTransactionTab/index.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import WalletTransactionTab from ".";
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+
+describe("WalletTransactionTab", () => {
+  const mockProps = {
+    currencyLogo: "path/to/logo.png",
+    currencyName: "Bitcoin",
+    userDescription: "User description",
+    currency: 10,
+    marketCap: 1000,
+    date: new Date("2023-01-01"),
+    chiplabel: "Label",
+  };
+
+  test("Renders transaction details correctly", () => {
+    render(<WalletTransactionTab {...mockProps} />);
+
+    expect(screen.getByText("Jan")).toBeInTheDocument();
+    expect(screen.getByText("1")).toBeInTheDocument();
+    expect(screen.getByAltText("icon")).toBeInTheDocument();
+    expect(screen.getByText("Bitcoin")).toBeInTheDocument();
+    expect(screen.getByText("User description")).toBeInTheDocument();
+    expect(screen.getByText("+10 BTC")).toBeInTheDocument();
+    expect(screen.getByText("+$1000")).toBeInTheDocument();
+    expect(screen.getByText("Label")).toBeInTheDocument();
+  });
+});
---
+++ frontend/src/components/molecules/walletTransactionTab/index.tsx
@@ -0,0 +1,129 @@
+import { Box } from "@mui/material";
+import React from "react";
+import MuiTypography from "../../atoms/typography";
+import IconComponent from "../../atoms/icon";
+import theme from "../../../theme";
+import styled from "@emotion/styled";
+import ChipItem from "../../atoms/chip"
+
+interface PortfolioProps {
+  currencyLogo: string;
+  currencyName: string;
+  userDescription: string;
+  currency: React.ReactNode;
+  marketCap: React.ReactNode;
+  date: Date;
+  chiplabel: string;
+}
+
+const StyledBox = styled(Box)({
+  color: theme.palette.structural.main,
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+  justifyContent: "space-between",
+  padding: "16px 24px",
+  background: theme.palette.structural.main,
+  maxWidth: "1239x",
+});
+
+const CurrencyStyle = styled(Box)({
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: "10px",
+  minWidth: "150px",
+});
+const ChipBox = styled(Box)({
+    paddingTop: '23px',
+  })
+const LeftPart = styled(Box)({
+  display:'flex',
+  flexDirection:'row',
+  gap:'10px',
+  })
+const RightPart = styled(Box)({
+    display:'flex',
+    flexDirection:'column',
+    alignItems:'flex-end',
+    justifyContent:'flex-end'
+    })
+const Divider = styled.hr`
+    Width: 1164px;
+    border: 1px solid ${theme.palette.grey[100]};
+    order: 1;
+  `;
+
+const WalletTransactionTab = ({
+  currencyLogo,
+  currencyName,
+  userDescription,
+  currency,
+  marketCap,
+  date,
+  chiplabel
+}: PortfolioProps) => {
+  const formattedDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "short",
+    year: "numeric"
+  });
+  const month = formattedDate.split(" ")[0];
+  const day = formattedDate.split(" ")[1].replace(",", "");
+
+  return (
+    <Box>
+    <StyledBox>
+      <LeftPart>
+        <Box>
+        <MuiTypography
+              variant="caption2"
+              text={month}
+              color={theme.palette.textColor.mediumEmphasis}
+            />
+            <MuiTypography
+              variant="subtitle2"
+              text={day}
+              color={theme.palette.textColor.highEmphasis}
+            />
+
+        </Box>
+        <CurrencyStyle>
+            <IconComponent src={currencyLogo} />
+            <Box>
+              <MuiTypography
+                variant="body1"
+                text={currencyName}
+                sx={{ color: theme.palette.textColor.highEmphasis }}
+              />
+              <MuiTypography
+                variant="caption2"
+                text={userDescription}
+                sx={{ color: theme.palette.textColor.mediumEmphasis }}
+              />
+            </Box>
+        </CurrencyStyle>
+        <ChipBox>
+          <ChipItem label={chiplabel} chipType={"rounded"} chipVariant="light" />
+        </ChipBox>
+      </LeftPart>
+      <RightPart>
+        <MuiTypography
+          variant="body1"
+          text={`+${currency} BTC`}
+          sx={{ color: theme.palette.textColor.highEmphasis}}
+        />
+        <MuiTypography
+          variant="caption2"
+          text={`+$${marketCap}`}
+          sx={{ color: theme.palette.textColor.mediumEmphasis}}
+        />
+      </RightPart>
+    </StyledBox>
+    <Divider />
+    </Box>
+  );
+};
+
+export default WalletTransactionTab;
