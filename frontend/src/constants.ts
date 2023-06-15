b4859d61f7eff62f3f204b9b307230f9cc3506bc
Shubham Raut <shubhamrajkumar.raut@zemosolabs.com>
2023-06-15 14:13:36 +0530

    feat: add TimePeriod molecule

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -10,3 +10,5 @@ export const menuItems = [
   export const buttonLabel = "NEED HELP";
    export const  buy="BUY";
    export const sell="SELL";
+
+export const timeLineValues = ["1H", "24H", "1W", "1M", "1Y", "ALL"];   
